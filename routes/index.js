0ac7393f5c5c810695454bbd039068d388a30031
tvanrys <tvanrys@fanshaweonline.ca>
2020-03-30 18:16:20 -0400

    trying some changes

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,30 @@ var express = require('express');
 var connect = require('../utils/sqlConnect');
 var router = express.Router();
 
-
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  console.log('get all the movies');
-  connect.query(`SELECT * FROM tbl_movies`, (error, rows) => {
-    if (error) {
-      //throw error;
-      console.log(error);
-    } else {
-      console.log('rows:', rows);
+  connect.getConnection((err, connection) => {
+    console.log('get all the movies');
 
-      res.render('home', {
-        defaultMovie: rows[Math.floor(Math.random() * rows.length)],
-        data: rows
-      });
-    }
+    connection.query(`SELECT * FROM tbl_movies`, (error, rows) => {
+      connection.release();
+
+      if (error) {
+        //throw error;
+        console.log(error);
+      } else {
+        //console.log('rows:', rows);
+
+        var currentMovie = rows[Math.floor(Math.random() * rows.length)];
+        console.log("current movie:", currentMovie);
+
+        res.render('home', {
+          defaultMovie: rows[Math.floor(Math.random() * rows.length)],
+          data: rows
+        });
+      }
+    });
   });
 });
 
