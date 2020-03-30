2ae0a9d025266c9f7a1a3b6bc474fbf7004174fa
tvanrys <tvanrys@fanshaweonline.ca>
2020-03-30 17:39:54 -0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,19 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
   console.log('get all the movies');
-  connect.query(`SELECT * FROM tbl_movies`, (error, rows)=> {
+  connect.query(`SELECT * FROM tbl_movies`, (error, rows) => {
     if (error) {
       //throw error;
       console.log(error);
     } else {
       console.log('rows:', rows);
 
-      res.render('home', {        
-        defaultMovie : rows[Math.floor(Math.random() * rows.length)],
-        data : rows
+      res.render('home', {
+        defaultMovie: rows[Math.floor(Math.random() * rows.length)],
+        data: rows
       });
     }
   });
